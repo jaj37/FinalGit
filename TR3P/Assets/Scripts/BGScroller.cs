9c0f7aff3077ccffae2ea22575599b4e3f21240e
jaj37 <47665114+jaj37@users.noreply.github.com>
2019-04-23 21:15:37 -0400

    Stuff

---
+++ TR3P/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeZ;
+	private bool faster;
+
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+		faster = false;
+		startPosition = transform.position;
+	}
+
+
+	void Update ()
+	{
+			if (Input.GetKeyDown(KeyCode.T))
+		{
+		faster = true;
+		}
+		if(faster)
+		{
+			if (scrollSpeed > -16)
+			scrollSpeed -= Time.deltaTime;
+		}
+		
+		
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
---
+++ TR3P/Assets/Scripts/GameController.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+
+    public Text gameOverText;
+    public Text restartText;
+    public Text scoreText;
+	public Text winText;
+
+
+	private bool win;
+    private bool gameOver;
+	private bool restart;
+	private int score;
+
+		public bool timeAttack;
+		float timeLeft = 30.0f;
+		public Text text;
+	
+	void Start ()
+	{
+
+		win = false;
+		gameOver = false;
+		restart = false;
+		winText.text = "";
+		restartText.text = "";
+		gameOverText.text = "";
+		text.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+
+	}
+	
+	void Update ()
+	{
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.F))
+			{
+                SceneManager.LoadScene("Final");
+            }
+		}
+				if (Input.GetKeyDown(KeyCode.T))
+		{
+		timeAttack = true;
+		}
+		if (timeAttack){
+		 timeLeft -= Time.deltaTime;
+         text.text = "Time Left: " + Mathf.Round(timeLeft);
+		}
+		         if(timeLeft < 0)
+         {
+				timeLeft = 0f;
+				winText.text = "You Win! Game Created by Jason Johnson";
+				win = true;
+         }
+		
+			
+	}
+	
+	
+	
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+			if (win)
+			{
+				restartText.text = "Press 'F' for Restart";
+				restart = true;
+
+				break;
+			}
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'F' for Restart";
+				restart = true;
+
+				break;
+			}
+		}
+	}
+	
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+
+	}
+	
+	void UpdateScore ()
+	{
+		scoreText.text = "Points: " + score;
+		if(timeAttack == false){
+		if (score >= 100) {
+		winText.text = "You Win! Game Created by Jason Johnson";
+		win = true;
+		}
+		}
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over :(";
+		gameOver = true;
+	}
+}
